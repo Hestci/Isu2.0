adcfc697ebbf3c11ce665fc3840aa9ac182bdf1b
Hest <32201804+Hestci@users.noreply.github.com>
2025-03-09 17:19:17 +0300

    Add files via upload

---
+++ isu/myapp/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import RedirectView
+from myapp import views
+
+urlpatterns = [
+    path('lk/', views.lk, name='lk'),
+    path('', views.user_login, name='home'),  # Теперь корневой URL ведет на страницу входа
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),  # Оставьте этот маршрут для явного доступа к странице входа
+]
