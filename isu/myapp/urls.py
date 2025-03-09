91b6b660247493e46839e6ba97466e8dc06954e7
МОЁ-ИМЯ <ewewe@yandex.ru>
2025-03-09 18:30:54 +0300

    Допилил логику

--- isu/myapp/urls.py
+++ isu/myapp/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('', views.user_login, name='home'),  # Теперь корневой URL ведет на страницу входа
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),  # Оставьте этот маршрут для явного доступа к странице входа
+    path('logout/', views.user_logout, name='logout'),  # Новый маршрут
+    path('unauthorized/', views.unauthorized, name='unauthorized'),
 ]
+
+
--- isu/myapp/views.py
+++ isu/myapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from .forms import RegisterForm, LoginForm
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm, LoginForm
 
 def register(request):
     if request.method == "POST":
@@ -10,12 +10,17 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("home")  # Перенаправление на главную страницу после регистрации
+            messages.success(request, "Регистрация прошла успешно!")
+            return redirect("home")
+        else:
+            messages.error(request, "Ошибка в данных формы.")
     else:
         form = RegisterForm()
     return render(request, "register.html", {"form": form})
 
 def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('lk')
     if request.method == "POST":
         form = LoginForm(request, data=request.POST)
         if form.is_valid():
@@ -24,11 +29,24 @@ def user_login(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('lk')  # Перенаправление на личный кабинет
+                messages.success(request, "Вы успешно вошли!")
+                return redirect('lk')
+            else:
+                messages.error(request, "Неверное имя пользователя или пароль.")
+        else:
+            messages.error(request, "Ошибка в данных формы.")
     else:
         form = LoginForm()
     return render(request, "login.html", {"form": form})
 
-@login_required
+@login_required(login_url='/unauthorized/')
 def lk(request):
-    return render(request, 'lk.html')
+    return render(request, 'lk.html', {'user': request.user})
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, "Вы успешно вышли из аккаунта.")
+    return redirect('login')
+
+def unauthorized(request):
+    return render(request, 'unauthorized.html')
