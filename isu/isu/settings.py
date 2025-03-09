7b296317bbea707d6bdcf9ed2ea7e7d6f8c234b5
МОЁ-ИМЯ <ewewe@yandex.ru>
2025-03-09 19:37:20 +0300

    для хостинга изменил значение ALLOWED_HOSTS

--- isu/isu/settings.py
+++ isu/isu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7@6-x6d7_n_%4c11g18iwkw%xbptdk&p!bw=bnb5gm#8-othbh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
