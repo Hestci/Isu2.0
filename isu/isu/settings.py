16eda443c2065be7ee12dab6b3b0d03e8a87f76d
МОЁ-ИМЯ <ewewe@yandex.ru>
2025-04-16 17:30:45 +0300

    Set DEBUG to False in settings.py to enhance production readiness.

--- isu/isu/settings.py
+++ isu/isu/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7@6-x6d7_n_%4c11g18iwkw%xbptdk&p!bw=bnb5gm#8-othbh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = ['*']  # Временно разрешаем все хосты
 
